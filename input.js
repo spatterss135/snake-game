a2392605553b86f4ec7c78d6df85d7002ae4d98f
spatterss135 <spatterss135@gmail.com>
2021-09-22 10:12:24 -0500

    Added instructions and audio

--- input.js
+++ input.js
@@ -1,10 +1,13 @@
+const audio = document.querySelector('audio')
+const instructions = document.querySelector('#instructions')
 let inputDirection = {x: 0, y: 0}
 
 
 
 
 document.addEventListener('keydown', e => {
-    
+    instructions.style.opacity = '0'
+    audio.play()
     switch (e.key) {
         case 'ArrowUp':
             if (inputDirection.y === 0){
